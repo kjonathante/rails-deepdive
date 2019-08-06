22d3ad979aa2d68ba1788da5292d88e9d940aaf7
Kit Jonathan Te <kjonathante@gmail.com>
2019-08-05 20:07:04 -0700

    Added a users controller and new action. Modified test. Added signup route for users/new route.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   # get 'static_pages/about'
   get '/about', to: 'static_pages#about'
+  # get 'users/new'
+  get '/signup', to: 'users#new'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#search'
 end
